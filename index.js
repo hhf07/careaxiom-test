3ef3f4a9b7ef96b598b41c6adfd1d0e06b15e056
Hamza Hamid <hamza.hamid@southvillesol.com>
2021-10-21 18:05:20 +0500

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require("path");
+const axios = require('axios');
 
 
 const app = express();
@@ -8,10 +9,25 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.get('/?', async (req, res) => {
-    res.status(200).render("index", { websites: typeof req.query.address === 'string' ? [req.query.address] : req.query.address })
+    let data = [];
+    let addresses = typeof req.query.address === 'string' ? [req.query.address] : req.query.address;
+    for (address of addresses) {
+        await urlCheck(!address.includes('http') ? "http://" + address : address) ? data.push(address) : data.push(`${address} - NO RESPONSE`);
+    }
+    res.status(200).render("index", { websites: data })
 });
 
 app.get('*', (req, res) => res.status(404).render("error"));
 
 
 app.listen(port, () => console.log(`Server listening on PORT ${port}`));
+
+
+const urlCheck = async (url) => {
+    try {
+        await axios.head(url);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
