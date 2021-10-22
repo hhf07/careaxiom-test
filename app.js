4aa69429ebb47f3b46d11c931a52d40ca3167c22
Hamza Hamid <hamza.hamid@southvillesol.com>
2021-10-22 19:40:47 +0500

    Added app

--- app.js
+++ app.js
@@ -10,15 +10,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get('/?', async (req, res) => {
     let addresses = typeof req.query.address === 'string' ? [req.query.address] : req.query.address;
-    // for (address of addresses) {
-    //     let title = await urlCheck(!address.includes('http') ? "http://" + address : address);
-    //     data.push( title ? `${address} - "${title}"` : `${address} - NO RESPONSE`);
-    // }
-    // urlCheck(addresses).then(data => res.status(200).render("index", { websites: data }))
-    urlCheck(addresses, data => {
-        res.status(200).render("index", { websites: data })
-    });
-
+    fetchTitles(addresses, data => res.status(200).render("index", { websites: data }));
 });
 
 app.get('*', (req, res) => res.status(404).render("error"));
@@ -27,20 +19,18 @@ app.get('*', (req, res) => res.status(404).render("error"));
 app.listen(port, () => console.log(`Server listening on PORT ${port}`));
 
 
-const urlCheck = async (addresses, callback) => {
+const fetchTitles = async (addresses, callback) => {
     let titles = [];
     for (let address of addresses) {
         try {
             let data = await axios.get(!address.includes('http') ? "http://" + address : address)
             let title = data.data.match(/<title[^>]*>([^<]+)<\/title>/)[1];
             titles.push(title ? `${address} - "${title}"` : `${address} - NO RESPONSE`);
-            // })
         } catch (error) {
             titles.push(`${address} - NO RESPONSE`);
         }
     }
     callback(titles);
-    // return titles;
 }
 
 //https://stackoverflow.com/questions/45620694/how-to-return-response-of-axios-in-return
